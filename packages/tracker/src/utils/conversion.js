e854e46257d37d231b695dd8ea24e27266aebdce
Kouba Jiří <jiri.kouba@generaliceska.cz>
2024-11-29 01:11:10 +0100

    fix wrong assignments, conversion from wei to ether

--- packages/tracker/src/utils/conversion.js
+++ packages/tracker/src/utils/conversion.js
@@ -1,10 +1,15 @@
 import { fromWei } from "web3-utils";
 import { toBitcoin } from "satoshi-bitcoin";
 
+/**
+ *
+ * @param {BigInt} sats
+ * @returns string
+ */
 export const satsToBtc = (sats) => {
-  return toBitcoin(sats).toString(10);
+  return toBitcoin(sats.toString(10)).toString(10);
 };
 
-export const weiToEther = (wei) => {
-  return fromWei(wei, "ether");
+export const weiToEther = (wei = 0) => {
+  return fromWei(wei, "ether").toString();
 };
