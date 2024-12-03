7bb1452db7cddfe786cec6514078de38c90b6716
Kouba Jiří <jiri.kouba@generaliceska.cz>
2024-12-03 20:27:22 +0100

    cache recently sent emails

--- packages/tracker/src/email/sendEmails.js
+++ packages/tracker/src/email/sendEmails.js
@@ -1,5 +1,8 @@
 import sgMail from "@sendgrid/mail";
 import { db } from "../config/firebase.js";
+import NodeCache from "node-cache";
+
+const cache = new NodeCache({ stdTTL: 10 * 60 });
 
 export const mailToUsers = async (address, coll, property = "emails") => {
   const docSnap = await db.collection(coll).doc(address).get();
@@ -7,12 +10,14 @@ export const mailToUsers = async (address, coll, property = "emails") => {
 };
 
 export const sendEmails = async (emails = []) => {
-  if (emails.length === 0) return;
+  const emailsToSend = emails.filter((email) => !cache.has(email));
+  if (emailsToSend.length === 0) return;
+  cache.mset(emailsToSend.map((email) => ({ [email]: email })));
   try {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const response = await sgMail.sendMultiple({
       from: "cryptotracker65@gmail.com",
-      to: emails,
+      to: emailsToSend,
       subject: "Crypto Tracker Alert",
       templateId: process.env.SENDGRID_TEMPLATE_ID,
     });
