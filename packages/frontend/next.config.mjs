5b826ab6a32e6303769a84b9e3214e7765fa30cd
Kouba Jiří <jiri.kouba@generaliceska.cz>
2024-12-04 04:00:54 +0100

    appCheck config

--- packages/frontend/next.config.mjs
+++ packages/frontend/next.config.mjs
@@ -18,6 +18,19 @@ const nextConfig = {
             key: "Cross-Origin-Opener-Policy",
             value: "same-origin same-origin-allow-popups",
           },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
         ],
       },
     ];
